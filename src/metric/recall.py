cddd399bd9017dc2711219bfa64f85e944ea5642
Joey De Pauw <joeydepauw@gmail.com>
2021-05-20 12:04:43 +0200

    fix critical bug in recall metric

--- src/metric/recall.py
+++ src/metric/recall.py
@@ -12,6 +12,6 @@ def recall_k(predictions: np.ndarray, Xval_out: scipy.sparse.csr_matrix, top_k):
     hits = np.take_along_axis(Xval_out, recommendations, axis=1)
     total_hits = np.asarray(hits.sum(axis=1)).flatten()
     best_possible = np.asarray(Xval_out.sum(axis=1)).flatten()
-    best_possible[best_possible < top_k] = top_k
+    best_possible[best_possible > top_k] = top_k
     recall_scores = total_hits / best_possible
     return recall_scores
